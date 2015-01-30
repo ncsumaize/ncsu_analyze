c0762b4f05c0431a94f7fd4a6a8a5c47c0411c44
ncsumaize <ncsumaize@gmail.com>
2015-01-30 10:14:23 -0500

    commented out stuff at end

--- analyze.py
+++ analyze.py
@@ -22,6 +22,6 @@ def analyze(filename):
     plt.tight_layout()
     plt.show()
 
-script = sys.argv[0] #this tells the program to read in arguments from the command line when this script is run, the 1st argument (index = 0) is the script name
-inputfile = sys.argv[1] #2nd argument when calling the script in bash is input file name
-outputfile = sys.argv[2] #3rd argument is the output file name
+# script = sys.argv[0] #this tells the program to read in arguments from the command line when this script is run, the 1st argument (index = 0) is the script name
+#inputfile = sys.argv[1] #2nd argument when calling the script in bash is input file name
+#outputfile = sys.argv[2] #3rd argument is the output file name
